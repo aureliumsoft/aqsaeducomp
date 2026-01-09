89cbbd1c7a133e3fe9d82ff6a8b37dab6124a654
ammarwebdev0001 <ammaryounas0001@gmail.com>
2026-01-09 17:38:01 +0500

    Update RenderFeaturedCourses.tsx

--- app/(endusers)/_compoments/RenderFeaturedCourses.tsx
+++ app/(endusers)/_compoments/RenderFeaturedCourses.tsx
@@ -17,7 +17,7 @@ export async function RenderFeaturedCourses() {
         />
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
-          {courses.map((course) => (
+          {courses.map((course: any) => (
             <PublicCourseCard key={course.id} data={course} />
           ))}
         </div>
