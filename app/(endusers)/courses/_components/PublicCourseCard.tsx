1d83d9bb8c1bc68f25440d12da8f3b38d9653f93
ammarwebdev0001 <ammaryounas0001@gmail.com>
2026-01-09 17:41:40 +0500

    fixes3 v-1.2

--- app/(endusers)/courses/_components/PublicCourseCard.tsx
+++ app/(endusers)/courses/_components/PublicCourseCard.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { PublicCourseType } from "@/app/data/course/get-all-courses";
 import { Badge } from "@/components/ui/badge";
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
@@ -12,7 +11,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface iAppProps {
-  data: PublicCourseType;
+  data: any;
 }
 
 export function PublicCourseCard({ data }: iAppProps) {
